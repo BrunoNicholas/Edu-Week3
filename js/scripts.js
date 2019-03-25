75c1865404fb8678277e4e920f2e43f3cb4948fc
Bruno Nicholas <sbnibro256@gmail.com>
2019-03-25 17:52:06 +0300

    added the colors in JQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+/*
 $(document).ready(function() {
   $("button#green").click(function() {
     $("body").addClass("green-background");
@@ -9,5 +11,39 @@ $(document).ready(function() {
 
   $("button#red").click(function() {
     $("body").addClass("red-background");
+  });
+});
+*/
+
+/*
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
+*/
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").css("background-color", "green");
+  });
+
+  $("button#yellow").click(function() {
+  	$("body").css("background-color", "yellow");
+  });
+
+  $("button#red").click(function() {
+  	$("body").css("background-color", "red");
   });
 });
